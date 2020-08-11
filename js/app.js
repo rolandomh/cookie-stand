9092ee3e24380a7c54cf81e5212632ed9d73fb8e
RolandoMH <ro@spacelabforever.com>
2020-08-10 18:14:03 -0700

    almostBuddy

---
+++ js/app.js
@@ -0,0 +1,56 @@
+"use strict"
+
+
+//Store Hours
+var storeHoursArr = [6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm]
+
+
+var Seattle = {
+  min: 23
+  max:65
+  avg:6.3
+}
+
+
+var Tokyo = {
+  min: 3
+  max:24
+  avg:1.2
+}
+
+
+var Dubai = {
+  min: 11
+  max:38
+  avg:3.7
+}
+
+
+var Paris = {
+  min: 20
+  max:38
+  avg:2.3
+}
+
+
+var Lima = {
+  min: 2
+  max:16
+  avg:4.6
+}
+
+
+
+function getRandomInt(min, max) {
+    return Math.ceil (math.random() * (100) +1);
+    
+}
+
+
+
+// random number gen math.random();
+function numOfGuests(min.max){
+  return Math.ceil(math.random() * (max - min)+min)
+}
+
+
