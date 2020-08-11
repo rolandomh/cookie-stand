204715a75226811e178ed8c8ba4af8dedde96293
RolandoMH <ro@spacelabforever.com>
2020-08-10 22:24:50 -0700

    thirdGo

--- js/app.js
+++ js/app.js
@@ -1,56 +1,60 @@
 "use strict"
 
-
-//Store Hours
-var storeHoursArr = [6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm]
-
-
+// random number gen Math.random();
+function numOfGuests(min , max){
+  return Math.ceil(Math.random() * (max - min) + min)
+}
+//Store Hours var nameArr = []
+var storeHoursArr = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"]
+//  console.log(storeHoursArr[i], sales(guesttest, 61.2));
+//func equation calculate guests multiplied by average cookie sale 
+function sales(guestsPerHour, avgCookieSale) {
+  return guestsPerHour * avgCookieSale;
+} 
+function hourlySales(min, max, avgCookieSale){
+for (var i = 0; i < 14; i++) {
+  var seaCalc = numOfGuests(min, max)
+   console.log (sales (seaCalc, avgCookieSale))}}
+//cookies purchased each hour by store
 var Seattle = {
-  min: 23
-  max:65
-  avg:6.3
+  min: 23,
+  max: 65,
+  avgCookieSale: 6.3,
 }
-
-
+function hourlySales(min, max, avgCookieSale){
+  for (var i = 0; i < 14; i++) {
+    var tokyoCalc = numOfGuests(min, max)
+     console.log (sales ( tokyoCalc, avgCookieSale))}}
+  //cookies purchased each hour by store
 var Tokyo = {
-  min: 3
-  max:24
-  avg:1.2
+  min: 3,
+  max: 24,
+  avgCookieSale :1.2,
 }
-
-
+function hourlySales(min, max, avgCookieSale){
+  for (var i = 0; i < 14; i++) {
+    var dubaiCalc = numOfGuests(min, max)
+     console.log (sales ( dubaiCalc, avgCookieSale))}}
 var Dubai = {
-  min: 11
-  max:38
-  avg:3.7
+  min: 11,
+  max: 38,
+  avgCookieSale: 3.7,
 }
-
-
+function hourlySales(min, max, avgCookieSale){
+  for (var i = 0; i < 14; i++) {
+    var parisCalc = numOfGuests(min, max)
+     console.log (sales ( parisCalc, avgCookieSale))}}
 var Paris = {
-  min: 20
-  max:38
-  avg:2.3
+  min: 20,
+  max: 38,
+  avgCookieSale: 2.3,
 }
-
-
+function hourlySales(min, max, avgCookieSale){
+  for (var i = 0; i < 14; i++) {
+    var limaCalc = numOfGuests(min, max)
+     console.log (sales ( limaCalc, avgCookieSale))}}
 var Lima = {
-  min: 2
-  max:16
-  avg:4.6
+  min: 2,
+  max: 16,
+  avgCookieSale: 4.6,
 }
-
-
-
-function getRandomInt(min, max) {
-    return Math.ceil (math.random() * (100) +1);
-    
-}
-
-
-
-// random number gen math.random();
-function numOfGuests(min.max){
-  return Math.ceil(math.random() * (max - min)+min)
-}
-
-
