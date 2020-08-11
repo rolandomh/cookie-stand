33ee002590d9e1d8094022d15248b7b029256cfd
RolandoMH <ro@spacelabforever.com>
2020-08-10 23:14:56 -0700

    last push for the night

--- js/app.js
+++ js/app.js
@@ -58,3 +58,9 @@ var Lima = {
   max: 16,
   avgCookieSale: 4.6,
 }
+
+
+//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+// Store the results for each location in a separate array… perhaps as a property of the object representing that location
+// Display the values of each array as unordered lists in the browser
+// Calculating the sum of these hourly totals; your output for each location should look like this:
